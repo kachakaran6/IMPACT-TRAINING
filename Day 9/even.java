7db93b4501b775687f2fb9e3a8bdb48547cf26a0
kachakaran6 <kachakaran06@gmail.com>
2025-05-28 20:35:34 +0530

    even

---
+++ Day 9/even.java
@@ -0,0 +1,8 @@
+public class even {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 0; i <= n; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
