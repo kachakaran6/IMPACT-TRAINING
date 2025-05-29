bcc99ec629d084203b987a1e6b7cbbaef799e435
kachakaran6 <kachakaran06@gmail.com>
2025-05-29 22:51:06 +0530

    foundation 3 starts

---
+++ Foundation-3/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // 1 Mathematical Operations
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+
+        // System.out.println(a+b);
+        // System.out.println(a-b);
+        // System.out.println(a*b);
+        // System.out.println(a/b);
+        // System.out.println(a%b);
+    }
+}
