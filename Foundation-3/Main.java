ef93c5585e8d4fc71699e3bd0c5e6714dd55a2c5
kachakaran6 <kachakaran06@gmail.com>
2025-05-29 23:11:56 +0530

    foundation 3

--- Foundation-3/Main.java
+++ Foundation-3/Main.java
@@ -11,5 +11,56 @@ public class Main {
         // System.out.println(a*b);
         // System.out.println(a/b);
         // System.out.println(a%b);
+
+        // 2 Agent Binod
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+
+        // System.out.println("Hi my name is Agent " + name);
+
+        // sc.close();
+
+        // 3 Circle Area-Perimeter
+        // Scanner sc = new Scanner(System.in);
+
+        // long r = sc.nextInt();
+        // long pi = 3;
+
+        // long area = pi * r * r;
+        // long perimeter = 2 * pi * r;
+
+        // System.out.println(area);
+        // System.out.println(perimeter);
+
+        // 4 Simple Interest
+
+        // double p = sc.nextDouble();
+        // double t = sc.nextDouble();
+        // double r = sc.nextDouble();
+
+        // double simpleInterest = (p*t*r)/100.0;
+
+        // System.out.println("Simple interest = " + simpleInterest);
+
+        // Celsius to Fahrenheit
+
+        // Scanner sc = new Scanner(System.in);
+        // int tempInCelsius = sc.nextInt();
+
+        // int f = (tempInCelsius*9/5)+32;
+
+        // System.out.println(f);
+
+        // Celsius to Fahrenheit 2
+
+        // Scanner sc = new Scanner(System.in);
+        // int c = sc.nextInt();
+
+        // float f = (c * 9 / 5.0f) + 32f;
+
+        // System.out.printf("%.6f\n", f);
+
+        //
+
     }
 }
