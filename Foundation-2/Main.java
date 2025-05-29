c7fa8e21c4a771fac5d8826632e2b9c78df3b740
kachakaran6 <kachakaran06@gmail.com>
2025-05-29 22:47:55 +0530

    foundation 2 completes

--- Foundation-2/Main.java
+++ Foundation-2/Main.java
@@ -41,10 +41,48 @@
 // }
 // }
 
-// ////////////////////////////////////////////////////////////////////////////Variable
+// ////////////////////////////////////////////////////////////////////////////
+// Variable
 // Practice Question 2
 
 // float var = 10.45f;
 // var = 20.55f;
 
 // System.out.print(var);
+
+// /////////////////////////////////////////////////////////// Variable Practice
+// Question 3
+
+// int a =10;
+// int b = 20;
+// int c = a + b;
+
+// System.out.print(c);
+
+// ////////////////////////////////////////////////////////////// Variable
+// Practice Question 4
+
+// int a = 1042;
+// int b = 7;
+
+// int quo = a/b;
+// int rem = a%b;
+
+// System.out.println(quo + " " + rem);
+
+//////////////////////////////////////////////////////////////////////////// Varibal8
+
+// for(int i=1; i<=10; i++){
+// System.out.println("8 x " + i + " = " + 8*i );
+// }
+
+// ///////////////////////////////////////////////////////////////////////
+
+// double rating1 = Math.floor(10.5f);
+// double rating2 = Math.floor(12.5f);
+// double rating3 = Math.floor(12.5f);
+
+// double average = (rating1 + rating2 + rating3) / 3;
+// int finalResult = (int) Math.floor(average);
+
+// System.out.println(finalResult);
