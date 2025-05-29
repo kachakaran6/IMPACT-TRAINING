07aba9266ed2fbbbd96bfebb9dc75557e73eabf7
kachakaran6 <kachakaran06@gmail.com>
2025-05-29 22:31:50 +0530

    f2 going on

--- Foundation-2/Main.java
+++ Foundation-2/Main.java
@@ -1,10 +1,50 @@
-// Hello World
+/////////////////////////////////////////////////////////////////////////////////////////////// Hello World
 
-// import java.util.*;
-
-// public class Main {
 // public static void main(String[] args) throws Throwable {
 //
 // System.out.print("Hello World");
 //
+
+///////////////////////////////////////////////////////////////////////////////////////////// Star Pattern
+
+// int n = 3;
+// for(int i =1; i<=n; i++){
+
+// for(int j=1; j<=2*i-1; j++){
+// System.out.print("*");
 // }
+// System.out.println("");
+// }
+
+// ///////////////////////////////////////////////////////////////////////////////////
+// Print Mirror Z
+
+// public class Main {
+// public static void main(String[] args) {
+// int n = 6;
+
+// for (int i = 0; i < n; i++) {
+// if (i == 0 || i == n - 1) {
+
+// for (int j = 0; j < n; j++) {
+// System.out.print("*");
+// }
+// } else {
+
+// for (int j = 0; j < i; j++) {
+// System.out.print(" ");
+// }
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+// }
+// }
+
+// ////////////////////////////////////////////////////////////////////////////Variable
+// Practice Question 2
+
+// float var = 10.45f;
+// var = 20.55f;
+
+// System.out.print(var);
