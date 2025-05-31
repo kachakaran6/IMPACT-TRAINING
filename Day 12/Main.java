3d1b024d69bb26b4f97ced333737b8d5468c4942
kachakaran6 <kachakaran06@gmail.com>
2025-05-31 08:43:31 +0530

    star pattern

--- Day 12/Main.java
+++ Day 12/Main.java
@@ -36,10 +36,41 @@ public class Main {
         // }
         // Character Pattern
 
+        // for (int i = 1; i <= n; i++) {
+        // char ch = (char) ('A' + i - 1);
+        // for (int k = 1; k <= i; k++) {
+        // System.out.print(ch);
+        // }
+        // System.out.println();
+        // }
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // Diamond
+
+        // Top
         for (int i = 1; i <= n; i++) {
-            char ch = (char) ('A' + i - 1);
-            for (int k = 1; k <= i; k++) {
-                System.out.print(ch);
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Bottom
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
